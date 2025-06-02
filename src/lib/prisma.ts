faf69490505fbb76631ed87b9c2f7dc83f810e30
Fernando Arroyo <fer3443@gmail.com>
2025-06-02 14:04:58 -0300

    feat:PrismaNeon adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from '@/generated/prisma'
-import { withAccelerate } from '@prisma/extension-accelerate'
+// import { PrismaClient } from '@/generated/prisma'
+// import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
-}
+// const globalForPrisma = global as unknown as { 
+//     prisma: PrismaClient
+// }
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+// const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export default prisma
+// export default prisma
 
 
 // const globalForPrisma = global as unknown as { prisma: PrismaClient };
@@ -18,3 +18,27 @@ export default prisma
 //   globalForPrisma.prisma || new PrismaClient();
 
 // if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { neonConfig } from '@neondatabase/serverless';
+
+import ws from 'ws';
+neonConfig.webSocketConstructor = ws;
+
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+// neonConfig.poolQueryViaFetch = true
+
+// Type definitions
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+const prisma = global.prisma || new PrismaClient({adapter});
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export default prisma;
